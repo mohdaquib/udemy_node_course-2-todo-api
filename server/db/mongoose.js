a97e700ff439a38ca2c939947b29eb401513226b
Mohd Aquib <mohd.aquib09@gmail.com>
2017-11-27 01:41:09 +0530

    Modified mogoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://aquib:Mayajaal23@ds121896.mlab.com:21896/todo_app');
 
 module.exports = {mongoose};
