5e9dc6da0b5f45c7ebb1e3db2efccd564f2fc1f5
Mohd Aquib <mohd.aquib09@gmail.com>
2017-11-29 12:06:50 +0530

    Add DELETE /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://aquib:Mayajaal23@ds121896.mlab.com:21896/todo_app');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
